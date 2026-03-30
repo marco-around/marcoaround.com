50560fd0c895bb2db2e922f0b2f08bed6058f627
marco-trds <marcosdevprofessional@gmail.com>
2026-03-30 16:49:02 -0300

    feat: initial project setup

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from 'eslint/config'
+import eslintPluginAstro from 'eslint-plugin-astro'
+import eslintPluginReactHooks from 'eslint-plugin-react-hooks'
+import eslintPluginSimpleImportSort from 'eslint-plugin-simple-import-sort'
+import typescriptEslint from 'typescript-eslint'
+
+export default defineConfig([
+	{ ignores: ['dist/**', '.astro/**', 'node_modules/**'] },
+
+	...typescriptEslint.configs.recommended,
+	...eslintPluginAstro.configs.recommended,
+
+	{
+		files: ['**/*.{js,jsx,ts,tsx}'],
+		plugins: {
+			'react-hooks': eslintPluginReactHooks,
+			'simple-import-sort': eslintPluginSimpleImportSort,
+		},
+		rules: {
+			...eslintPluginReactHooks.configs.recommended.rules,
+			'simple-import-sort/imports': 'error',
+		},
+	},
+])
---
+++ src/components/react-component.tsx
@@ -0,0 +1,7 @@
+export function ReactComponent() {
+	return (
+		<div className='flex min-h-screen items-center justify-center'>
+			<h1 className='text-2xl font-bold'>React component on Astro</h1>
+		</div>
+	)
+}
